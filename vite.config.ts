35f72c9b8cafb97e089381ecd1d84485d20cac69
ajwwong <ajwwong@gmail.com>
2025-02-15 21:55:37 -0800

    server for https settings in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dns from 'dns';
 import path from 'path';
-import fs from 'fs';
-import path from 'path';
+
+//this is for the https server
+//import fs from 'fs';
+//import path from 'path';
 
 dns.setDefaultResultOrder('verbatim');
 
@@ -12,11 +14,12 @@ export default defineConfig({
   plugins: [react()],
   host: 'localhost',
   port: 3000,
-  server: {
+ ///this is for the https server
+ /* server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, '.cert/key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, '.cert/cert.pem')),
     },
     port: 5173
-  }
+  }*/
 });
